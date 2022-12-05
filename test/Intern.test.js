6bb2bf45f913808a1c24c4dd4bbec1aafaa4de26
Jessica Hickman <109389786+jhlynn95@users.noreply.github.com>
2022-12-05 15:29:33 -0800

    Update Intern.test.js

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -2,19 +2,19 @@ const Intern = require('../lib/Intern');
 
 describe('Intern test', () => {
     it('Intern class creates corresponding name', () => {
-        const intern = new Intern('Jalen', '11', 'Jalen@gmail.com', 'UCI');
-        expect(intern.getName()).toEqual('Jalen');
+        const intern = new Intern('', '11', '@gmail.com', '');
+        expect(intern.getName()).toEqual('');
     });
     it('Intern class creates corresponding id', () => {
-        const intern = new Intern('Jalen', '11', 'Jalen@gmail.com', 'UCI');
+        const intern = new Intern('', '11', '@gmail.com', '');
         expect(intern.getId()).toEqual('11');
     });
     it('Intern class creates corresponding email', () => {
-        const intern = new Intern('Jalen', '11', 'Jalen@gmail.com', 'UCI');
-        expect(intern.getEmail()).toEqual('Jalen@gmail.com');
+        const intern = new Intern('', '11', '@gmail.com', '');
+        expect(intern.getEmail()).toEqual('@gmail.com');
     });
     it('Intern class creates corresponding school', () => {
-        const intern = new Intern('Jalen', '11', 'Jalen@gmail.com', 'UCI');
-        expect(intern.getSchool()).toEqual('UCI');
+        const intern = new Intern('', '11', '@gmail.com', '');
+        expect(intern.getSchool()).toEqual('');
     });
 })

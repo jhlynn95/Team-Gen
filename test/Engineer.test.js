49b3584353b9f76157135151bbb3d89590644506
Jessica Hickman <109389786+jhlynn95@users.noreply.github.com>
2022-12-05 15:27:46 -0800

    Update Engineer.test.js

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -2,19 +2,19 @@ const Engineer = require('../lib/Engineer');
 
 describe('Engineer test', () => {
     it('Engineer class creates corresponding name', () => {
-        const engineer = new Engineer('Jalen', '11', 'Jalen@gmail.com', 'jalenSmith');
-        expect(engineer.getName()).toEqual('Jalen');
+        const engineer = new Engineer('', '11', '@gmail.com', '');
+        expect(engineer.getName()).toEqual('');
     });
     it('Engineer class creates corresponding id', () => {
-        const engineer = new Engineer('Jalen', '11', 'Jalen@gmail.com', 'jalenSmith');
+        const engineer = new Engineer('', '11', '@gmail.com', '');
         expect(engineer.getId()).toEqual('11');
     });
     it('Engineer class creates corresponding email', () => {
-        const engineer = new Engineer('Jalen', '11', 'Jalen@gmail.com', 'jalenSmith');
-        expect(engineer.getEmail()).toEqual('Jalen@gmail.com');
+        const engineer = new Engineer('', '11', '@gmail.com', '');
+        expect(engineer.getEmail()).toEqual('@gmail.com');
     });
     it('the github entered is a string', () => {
-        const engineer = new Engineer('Jalen', '11', 'Jalen@gmail.com', 'jalenSmith');
-        expect(engineer.getGithub()).toEqual('jalenSmith');
+        const engineer = new Engineer('', '11', '@gmail.com', '');
+        expect(engineer.getGithub()).toEqual('');
     });
     })
